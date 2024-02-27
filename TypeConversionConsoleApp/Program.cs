5f95b289cf6e5b6726fe291667e30a5466d2e3d2
Pierre Plourde <pierre@spartancsharp.net>
2024-02-27 09:29:08 -0500

    Create Code
    Capture a user's age from the Console and then identify how old they will be in 25 years, as well as how old they were 25 years ago. Print that information to the Console in natural language.

--- TypeConversionConsoleApp/Program.cs
+++ TypeConversionConsoleApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("What is your age: ");
+string? ageText = Console.ReadLine();
+
+bool isAgeValid = int.TryParse(ageText, out int age);
+
+int ageIn25 = age + 25;
+int age25Ago = age - 25;
+
+Console.WriteLine($"In 25 years, you will be {ageIn25} years old; 25 years ago, you were {age25Ago} years old.");
